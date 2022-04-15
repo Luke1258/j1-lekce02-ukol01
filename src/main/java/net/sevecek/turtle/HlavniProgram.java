4fd38256c8999aa124071e14507a67829b68e966
Lukas Zradula <lukas.zradula@sap.com>
2022-04-15 14:23:08 +0200

    1st part - Pig

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,40 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+
+        for (int i = 0; i < 4; i++) {
+            zofka.move(150.0);
+            zofka.turnRight(90.0);
+        }
+
+        zofka.turnLeft(60);
+        zofka.move(150.0);
+        zofka.turnRight(120.0);
+        zofka.move(150.0);
+
+        zofka.turnRight(30.0);
+        zofka.penUp();
+        zofka.move(150.0);
+        zofka.turnRight(90.0);
+        zofka.move(150.0);
+        zofka.turnRight(90.0);
+        zofka.penDown();
+
+        pigLegs(zofka);
+        zofka.turnLeft(45);
+        zofka.move(150);
+        pigLegs(zofka);
+    }
+
+    private void pigLegs(Turtle zofka) {
+        zofka.turnLeft(45);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnRight(90);
+        zofka.move(50);
+        zofka.turnLeft(180);
+        zofka.move(50);
     }
 
 }
