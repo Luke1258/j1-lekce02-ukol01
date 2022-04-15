743cd0fa76937abdb45be1e3f43f7e5715038a6d
Lukas Zradula <lukas.zradula@sap.com>
2022-04-15 16:34:53 +0200

    3rd part - sun, pig, houses

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -9,24 +9,74 @@ public class HlavniProgram {
 
         zofka = new Turtle();
 
-        nakresliPrasatko(zofka);
-        zofka.turnLeft(45);
-        zofka.penUp();
-        zofka.move(550);
-        zofka.penDown();
+//        nakresliPrasatko(zofka);
+//        zofka.turnLeft(45);
+//        zofka.penUp();
+//        zofka.move(550);
+//        zofka.penDown();
+//
+//        nakresliOsemuholnik(zofka);
+//        zofka.turnRight(180);
+//        zofka.penUp();
+//        zofka.move(100);
+//        zofka.penDown();
+//
+//        nakresliKolecko(zofka);
+//        zofka.penUp();
+//        zofka.move(200);
+//        zofka.penDown();
 
-        nakresliOsemuholnik(zofka);
-        zofka.turnRight(180);
-        zofka.penUp();
-        zofka.move(100);
-        zofka.penDown();
+//        nakresliSlnko(zofka);
 
-        nakresliKolecko(zofka);
-        zofka.penUp();
-        zofka.move(200);
-        zofka.penDown();
+        //Prva uroven - Slnko
+        zofka.setLocation(300,100);
         nakresliSlnko(zofka);
 
+        //Druha uroven - 5 x Domcek
+        zofka.setLocation(20, 500);
+        nakresliDomcek(zofka);
+        zofka.setX(300);
+        nakresliDomcek(zofka);
+        zofka.setX(600);
+        nakresliDomcek(zofka);
+        zofka.setX(900);
+        nakresliDomcek(zofka);
+        zofka.setX(1200);
+        nakresliDomcek(zofka);
+
+        //Tretia uroven - domcek, prasiatko, domcek
+        zofka.setLocation(20, 850);
+        nakresliDomcek(zofka);
+        zofka.setX(600);
+        nakresliPrasatko(zofka);
+        zofka.turnRight(45);
+        zofka.setX(1200);
+        nakresliDomcek(zofka);
+
+    }
+
+    private void nakresliDomcek(Turtle zofka) {
+        for (int i = 0; i < 4; i++) {
+            zofka.move(150.0);
+            zofka.turnRight(90.0);
+        }
+
+        zofka.penUp();
+        zofka.move(150.0);
+        zofka.penDown();
+
+        zofka.turnRight(30.0);
+        zofka.move(150.0);
+        zofka.turnRight(120.0);
+        zofka.move(150.0);
+
+        zofka.turnRight(30.0);
+        zofka.penUp();
+        zofka.move(150.0);
+        zofka.turnRight(90.0);
+        zofka.move(150.0);
+        zofka.turnRight(90.0);
+        zofka.penDown();
     }
 
     private void nakresliSlnko(Turtle zofka) {
